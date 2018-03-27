122eb2410257d598a94f7f1414007f026947fec0
Jony Ferreira dos Santos <jonyferreira@hotmail.com>
2018-03-27 17:22:30 -0300

    initial commit

--- src/test/java/br/com/jonyfs/MavenLibFolderResolverApplicationTests.java
+++ src/test/java/br/com/jonyfs/MavenLibFolderResolverApplicationTests.java
@@ -1,16 +1,38 @@
 package br.com.jonyfs;
 
+import javax.annotation.Resource;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.shell.Shell;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Import(TestApplicationRunner.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class MavenLibFolderResolverApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Resource
+    ApplicationContext applicationContext;
+
+    @Resource
+    private Shell shell;
+
+    @Test
+    public void test1verifyIfApplicationContextIsNotNull() {
+        assertThat(applicationContext).isNotNull();
+    }
+
+    @Test
+    public void test2verifyIfInitialCountIsZero() {
+        //assertThat(shell.evaluate(() -> "analyze ./target")).hasisEqualTo(0L);
+        shell.evaluate(() -> "analyze ./target");
+    }
 
 }
